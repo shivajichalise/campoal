c0346db9fbb42c758158085ee546a15ec159c2d1
Shivaji Chalise <respond2shivaji@gmail.com>
2024-06-12 16:55:16 +0545

    made hero component resuable

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,16 +4,32 @@ import PlatformUsers from "./components/platform-users"
 import BelieveInVoice from "./components/believe-in-your-voice"
 import OurValues from "./components/our-values"
 import OurStory from "./components/our-story"
+import hero1Image from "@/assets/hero.png"
+import hero2Image from "@/assets/our-vision.png"
 
 function App() {
+    const hero1 = {
+        heading: "World's Petition Platform",
+        description: "World's Petition Platform",
+        img: hero1Image,
+    }
+
+    const hero2 = {
+        heading: "Our Vision",
+        description:
+            "Our vision is to be the best petition platform in the world.",
+        img: hero2Image,
+    }
+
     return (
         <>
             <Navbar />
-            <Hero />
+            <Hero {...hero1} />
             <PlatformUsers />
             <BelieveInVoice />
             <OurValues />
             <OurStory />
+            <Hero {...hero2} />
         </>
     )
 }
--- frontend/src/components/hero.tsx
+++ frontend/src/components/hero.tsx
@@ -1,19 +1,19 @@
-import image from "@/assets/hero.png"
 import { H1 } from "./ui/h1"
 
-const Hero = () => {
+interface HeroProps {
+    heading: string
+    description: string
+    img: string
+}
+
+const Hero = ({ heading, description, img }: HeroProps) => {
     return (
         <div className="bg-secondaryBackground flex items-center justify-center">
             <div className="flex w-4/6 flex-col items-center justify-center pt-20 lg:pt-24">
-                <H1 text="World's Petition Platform" />
-                <p className="text-text-100 py-6 text-center">
-                    We started in 2016 with the radical idea that anyone,
-                    anywhere, should be able to easily and securely to start
-                    their own petition. Today, we offer a trusted and
-                    easy-to-use platform for social movement accross the world.
-                </p>
+                <H1 text={heading} />
+                <p className="text-text-100 py-6 text-center">{description}</p>
 
-                <img src={image} />
+                <img src={img} />
             </div>
         </div>
     )
